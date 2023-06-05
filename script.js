99d63b53960411e37080c783cdc63914e21ee89b
unknown <abu040810@gmail.com>
2023-06-06 04:06:29 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,286 @@
+/*
+/*
+switch case 1
+let num=4;
+switch(num){
+    case 1:console.log("dushanba");break;
+    case 2:console.log("seshanba");break;
+    case 3:console.log("chorshanba");break;
+    case 4:console.log("payshanba");break;
+    case 5:console.log("juma");break;
+    case 6:console.log("shanba");break;
+    case 7:console.log("yakshanba");break;
+    default:console.log("unknown");break;
+}
+
+*/
+/*
+switch case 2 masala
+let arg = prompt("enter value");
+switch (arg) {
+    case '1': console.log("yomon"); break;
+    case '2': console.log("qoniqarsiz"); break;
+    case '3': console.log("qoniqarli"); break;
+    case '4': console.log("yaxshi"); break;
+    case '5': console.log("alo"); break;
+    default: console.log("unknown"); break;
+}
+
+*/
+/* switch case 3 masala
+let arg = prompt("enter value");
+switch (arg) {
+    case '1': console.log("qish"); break;
+    case '2': console.log("qish"); break;
+    case '3': console.log("bahor"); break;
+    case '4': console.log("bahor"); break;
+    case '5': console.log("bahor"); break;
+    case '6': console.log("yoz"); break;
+    case '7': console.log("yoz"); break;
+    case '8': console.log("yoz"); break;
+    case '9': console.log("kuz"); break;
+    case '10': console.log("kuz"); break;
+    case '11': console.log("kuz"); break;
+    case '12': console.log("qish"); break;
+    default: console.log("unknown"); break;
+}
+*/
+
+/* switch case 4 masala
+
+let arg = prompt("enter value");
+switch (arg) {
+    case '1': console.log("31"); break;
+    case '2': console.log("28/29"); break;
+    case '3': console.log("31"); break;
+    case '4': console.log("30"); break;
+    case '5': console.log("31"); break;
+    case '6': console.log("30"); break;
+    case '7': console.log("31"); break;
+    case '8': console.log("31"); break;
+    case '9': console.log("30"); break;
+    case '10': console.log("30"); break;
+    case '11': console.log("30"); break;
+    case '12': console.log("31"); break;
+    default: console.log("unknown"); break;
+}
+
+*/
+/* switch case oxirrgi
+let arg = prompt("enter value");
+switch (arg) {
+    case '3': console.log("400"); break;
+    case '4': console.log("570"); break;
+    case '5': console.log("1120"); break;
+
+    default: console.log("unknown"); break;
+}
+*/
+//for begin//
+/* 1 masala(for)
+let a=10;
+let b=12;
+for(let i=0;i<a;i++){
+    console.log(b);
+}
+*/
+/* 2mmasala(for)
+let a=2;
+let b=6;
+if(a<b){
+    for (let i=a;i<=b;i++){
+        console.log(i);
+    }
+}
+*/
+/* 3masala(for)
+ let a=2;
+ let b=8;
+ if(b>a){
+     for (let i=b-1;i>=a;i--){
+         console.log(i);
+     }
+ }
+*/
+/*
+ 4 masala (for)
+ let a=10000;
+for(let i=1;i<=10;i++){
+    let b=a*i;
+     console.log(b);
+ }
+ */
+/*
+ 5masala for
+ let a = 10000;
+ for (let i = 0.1; i <=1; i=i+0.1) {
+     let b = a * i;
+     console.log(b);
+ }
+ 6 ham yuqoridagilar kabi algoritm ga ega i=1;i<=10;i=i+0.2 boladi faqat
+*/
+/*
+7 masala
+let a=5;
+let b=20;
+let sum=0;
+for (let i=a;i<=b;i++){
+    sum+=i;
+    console.log(sum);
+}
+*/
+
+/* 8 masala
+let a=5;
+let b=12;
+let sum=1;
+for (let i=a;i<=b;i++){
+    sum*=i;
+    console.log(sum);
+}
+*/
+/*
+9 masala
+let a=6;
+let b=12;
+let sum=0;
+for (let i=a;i<=b;i++){
+    sum+=(i*i);
+    console.log(sum);
+}
+
+*/
+/* 10 masala (for)
+let n=3;
+let sum=0;
+for (let i=1;i<=n;i++)
+{
+    sum+=(1/i);
+    //console.log ni kiritish mumkin shu yerda lekin unda takrorlanuvchi bolib qoladi
+}
+  console.log(sum);
+
+  */
+/* 11 masala
+let n=20;
+for (let i=0;i<n;i++){
+    if(i%2==1)
+    {
+        console.log(i);
+    }
+}
+*/
+/* 12 masala
+let n=20;
+for (let i=0;i<n;i++){
+    if(i%2==0)
+    {
+        console.log(i);
+    }
+}
+*/
+/* 13 masala(for)
+let n=25;
+let cnt=0;
+for (let i=2;i<=n;i=i+2){
+    for(let j=0;j<=i;j=j+1)
+    {
+        if(i%j==0)
+        { 
+            cnt+=1;
+
+        }
+     
+       
+    }
+    if(cnt>2)
+    {
+     console.log(i);
+    }
+     
+
+}
+*/
+
+/* 14 masala
+let n=20;
+for (let i=0;i<n;i++){
+     if(i%2==0)
+     {
+        console.log(i*i);
+     }
+     if(i%2==1)
+     {
+        console.log(i*i);
+     }
+       //yoki shunchaki console.log(i*i);
+    
+}
+
+*/
+/* 15 masala
+let n=20;
+for(let i=n;i>0;i--)
+{
+    console.log(i);
+}
+
+*/
+/* 16 masala
+let n=25;
+for(let i=0;i<20;i++)
+{
+    if(i%5!==0)
+    {
+        console.log(i);
+    }
+}
+*/
+/* 17 masala
+let n=15;
+let s=1;
+for(let i=1;i<=15;i++)
+{
+    s*=i;
+
+}
+console.log(s);
+
+*/
+/* 18 masala
+ let n = 3;
+ let count = 1;
+ let count1 = 0;
+ for (let i = 1; i<=n; i++){
+     for (let k = 1; k<=i; j++){
+        count *= k;
+     }
+     count1 += count
+ }
+ console.log(count1);
+ */
+/* 20 masala
+ let a=10;
+ let b=2;
+ for (let i=0;i<a;i++)
+ {
+    if(i%b==0)
+    {
+        console.log(i);
+    }
+ }
+*/
+
+/*
+ 24 masala
+let n = 10;
+let c = 1;
+for (let i = 1; i <= 10; i++) {
+    for (j = 1; j <= 10; j++) {
+        c = i * j;
+
+        console.log(`${i} * ${j} = ${c}`)
+    }
+    console.log("\n")
+}
+*/
